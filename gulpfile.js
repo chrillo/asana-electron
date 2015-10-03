23df3ef0df1a5115e8b2c0c6cdf74a0099ce9243
Christoph Peter <peter@internetkultur.at>
2015-10-03 10:33:25 +0200

    moved files around so the package.json is in the root folder

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var iconutil = require('gulp-iconutil')
 var childProcess = require('child_process')
 var fs = require('fs')
 
-var packageJson = JSON.parse(fs.readFileSync('./src/package.json'));
+var packageJson = JSON.parse(fs.readFileSync('./package.json'));
 
 gulp.task('icon', function(){
   gulp.src('./icon/icon_*.png')
@@ -13,7 +13,7 @@ gulp.task('icon', function(){
 
 gulp.task('build',['icon'],function(cb){
 
-  var build = 'electron-packager ./src ' + packageJson.name + ' --platform=darwin --arch=x64 --version=0.33.4 --icon=./icon/app.icns --overwrite --out ./release/' + packageJson.version + '/'
+  var build = 'electron-packager . ' + packageJson.name + ' --platform=darwin --arch=x64 --version=0.33.4 --icon=./icon/app.icns --overwrite --out ./release/' + packageJson.version + '/'
   ls = childProcess.exec(build, function (error, stdout, stderr) {
   if (error) {
     console.log(error.stack);
