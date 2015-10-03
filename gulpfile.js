b3e86f6c7e0ab8e42def4619bd59507fec64c09c
Christoph Peter <peter@internetkultur.at>
2015-10-03 11:10:17 +0200

    added .ico for windows builds

--- gulpfile.js
+++ gulpfile.js
@@ -2,29 +2,50 @@ var gulp     = require('gulp')
 var iconutil = require('gulp-iconutil')
 var childProcess = require('child_process')
 var fs = require('fs')
+var path = require('path');
+var im = require('imagemagick');
 
 var packageJson = JSON.parse(fs.readFileSync('./package.json'));
 
-gulp.task('icon', function(){
+gulp.task('icns', function(){
   gulp.src('./icon/icon_*.png')
     .pipe(iconutil('app.icns'))
     .pipe(gulp.dest('./icon/'))
 })
 
-gulp.task('build',['icon'],function(cb){
+gulp.task('ico', function(cb){
+  var input = './icon/icon_512x512.png'
+  var output = './icon/app.ico'
 
-  var build = 'electron-packager . ' + packageJson.name + ' --platform=darwin --arch=x64 --version=0.33.4 --icon=./icon/app.icns --overwrite --out ./release/' + packageJson.version + '/'
+  im.convert([input,
+      '-resize', '256x256',
+      '-gravity', 'center',
+      '-background', 'transparent',
+      '-flatten',
+      output
+  ], cb);
+})
+
+gulp.task('build',['icns','ico'],function(cb){
+
+  var build = 'electron-packager . ' + packageJson.name + ' --platform=darwin --arch=all --version=0.33.4 --icon=./icon/app --overwrite --out ./release/' + packageJson.version + '/'
   ls = childProcess.exec(build, function (error, stdout, stderr) {
   if (error) {
     console.log(error.stack);
     console.log('Error code: '+error.code);
     console.log('Signal received: '+error.signal);
   }
-  console.log('Child Process STDOUT: '+stdout);
-  console.log('Child Process STDERR: '+stderr);
+  cb()
+  //console.log('Child Process STDOUT: '+stdout);
+  //console.log('Child Process STDERR: '+stderr);
   });
-
+  ls.stdout.on('data', function(data) {
+    console.log(data.toString());
+  })
+  ls.stderr.on('data', function(data) {
+    console.error(data.toString());
+  })
   ls.on('exit', function (code) {
-    cb()
+
   })
 })
